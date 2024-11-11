25bd34f997a45c5e1845100f76b72339fbe04d51
Gelkoh <ole_kirchner@gmx.de>
2024-11-11 09:30:18 +0100

    Add filling kernel display table with DOM elements and values

--- static/script.js
+++ static/script.js
@@ -1,7 +1,39 @@
+// Convert JSON string to object
+const predefinedKernelsObj = JSON.parse(predefinedKernels)
+
 // Select the form to select filters
 const kernelSelectForm = document.getElementById("kernelSelectForm")
 const kernelSelect = document.getElementById("kernelSelect")
+const kernelDisplay = document.getElementById("kernelDisplay")
 const outputImage = document.getElementById("outputImg")
+
+const updateKernelDisplay = () => {
+    kernelDisplay.replaceChildren()
+
+    // Get the name of the selected kernel and it's weights
+    const selectedKernelName = kernelSelect.value
+    const selectedKernelWeights = predefinedKernelsObj[selectedKernelName]
+
+    // Create table rows/td elements to display the kernel
+    for (const row in selectedKernelWeights) {
+        const tableRow = document.createElement("tr")
+        
+        for (const col in selectedKernelWeights) {
+            const kernelWeightHolder = document.createElement("td") 
+            kernelWeightHolder.textContent = selectedKernelWeights[row][col]
+            tableRow.appendChild(kernelWeightHolder) 
+        }
+
+        kernelDisplay.appendChild(tableRow)
+    }
+}
+
+// Show the initial kernel weights of the initial kernel selection which is "IDENTITY"
+updateKernelDisplay()
+
+kernelSelect.addEventListener("change", () => {
+    updateKernelDisplay()
+})
 
 kernelSelectForm.addEventListener("submit", (event) => {
     // Prevent default action
